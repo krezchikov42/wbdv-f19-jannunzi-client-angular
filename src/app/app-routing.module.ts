5f8287e2bbb24c9be76e9d6f3f954e56e242700e
jannunzi <jannunzi@gmail.com>
2019-11-18 18:48:50 -0500

    added login, register, profile and navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {LoginComponent} from './login/login.component';
+import {RegisterComponent} from './register/register.component';
+import {CourseNavigatorComponent} from './course-navigator/course-navigator.component';
+import {ProfileComponent} from './profile/profile.component';
+import {AdminUsersComponent} from './admin-users/admin-users.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'profile', component: ProfileComponent},
+  {path: 'courses', component: CourseNavigatorComponent},
+  {path: 'admin-users', component: AdminUsersComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,30 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CourseNavigatorComponent } from './course-navigator/course-navigator.component';
 import {CourseService} from './services/CourseService';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ProfileComponent } from './profile/profile.component';
+import {FormsModule} from '@angular/forms';
+import {UserService} from './services/UserService';
+import { AdminUsersComponent } from './admin-users/admin-users.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CourseNavigatorComponent
+    CourseNavigatorComponent,
+    LoginComponent,
+    RegisterComponent,
+    ProfileComponent,
+    AdminUsersComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [
-    CourseService
+    CourseService,
+    UserService
   ],
   bootstrap: [AppComponent]
 })
