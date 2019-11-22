06bda077599de4db32bfa9d515dbb149c964c71c
krezchikov42 <rezchikov.k@husky.neu.edu>
2019-11-22 18:59:38 -0500

    adding routing to modules

--- src/app/module-list/module-list.component.ts
+++ src/app/module-list/module-list.component.ts
@@ -10,6 +10,7 @@ import {ModuleService} from '../services/ModuleService';
 export class ModuleListComponent implements OnInit {
   course_id = 1;
   module_many = [];
+  selected_module = {};
 
   constructor(private service: ModuleService, private router: Router, private activatedRoute: ActivatedRoute) {
   }
@@ -18,12 +19,17 @@ export class ModuleListComponent implements OnInit {
     // @ts-ignore
     this.activatedRoute.params
       .subscribe(params => {
-        this.course_id = params['cid']
+        this.course_id = params['cid'];
         this.service.findModulesForCourse(this.course_id).then(
           module_many => this.module_many = module_many
-        );;
+        );
+        ;
       });
 
   }
 
+  selectModule(module: any) {
+    this.selected_module = module;
+    this.router.navigate([this.router.url, module.id, 'lessons']);
+  }
 }
