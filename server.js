92c260afac29301c2296c4922beeb0fcd81057d4
krezchikov42 <rezchikov.k@husky.neu.edu>
2019-11-22 19:23:27 -0500

    deploy?

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
-app.use(express.static('./dist/wbdv-f19-jannuzi-client-angular/index.html'));
+app.use(express.static('./dist/wbdv-f19-jannuzi-client-angular'));
 app.get('/*', function (req, res) {
-  res.sendFile(__dirname + '/dist/index.html');
+  res.sendFile(__dirname + '/dist/wbdv-f19-jannuzi-client-angular/index.html');
 });
 app.listen(process.env.PORT || 8080);
